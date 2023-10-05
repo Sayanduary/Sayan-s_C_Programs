a65d89e1fa3dab73a8ddf348ee79f3e1ab39b545
Sayanduary <sayanduary@gmail.com>
2023-10-05 22:17:24 +0530

    Loops With C

---
+++ powers_of_sums.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int sum = 1, n, x;
+    printf("Enter The Value of X : \n");
+    scanf("%d", &x);
+    printf("Enter The Value of N : \n");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        sum += pow(x, i);
+    }
+    printf("The Sum is : %d \n", sum);
+
+    return 0;
+}
